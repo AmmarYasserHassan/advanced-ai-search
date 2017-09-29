beb67e15bd94296812ce681e147b4a4a6e1fe3bc
Omar_tarek <omar.tarek28@gmail.com>
2017-09-29 17:38:59 +0200

    ammar mawtny 3

--- src/project1/Grid.java
+++ src/project1/Grid.java
@@ -1,79 +1,84 @@
 package project1;
 
-public class Grid 
-{
-	
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Grid {
+
 	Cell[][] theGrid;
 	int length;
 	int width;
-	String[] possibleElements = {"Empty","Pad","Rock","Immovable","Teleportal"};
-	
+	String[] possibleElements = { "Empty", "Pad", "Rock", "Immovable" };
+	String[] mainElements = { "R2-D2", "Teleportal" };
 
-	
-	public Grid(int min, int max)
-	{
-		this.length = min + (int)(Math.random() * max); 
-		this.width = min + (int)(Math.random() * max); 
+	public Grid(int min, int max) {
+		// this.length = min + (int)(Math.random() * max);
+		// this.width = min + (int)(Math.random() * max);
+		this.length = min;
+		this.width = max;
 		this.theGrid = new Cell[this.length][this.width];
 	}
-	
-	
-	public void initializeGrid()
-	{
-		
-		boolean addedTeleportal = false;
+
+	public void initializeGrid() {
+
+		boolean teleportalAdded = false;
+		boolean R2D2Added = false;
+
+		// loops for adding the main elements (Teleportal, R2-D2)
+		while (!teleportalAdded && !R2D2Added) {
+			for (int i = 0; i < mainElements.length; i++) {
+				int newI = (int) (Math.random() * this.length);
+				int newJ = (int) (Math.random() * this.width);
+
+				String mainElement = this.mainElements[i];
+				if (this.theGrid[newI][newJ] == null) {
+
+					if (mainElement.equals("Teleportal"))
+						teleportalAdded = true;
+
+					if (mainElement.equals("R2-D2"))
+						R2D2Added = true;
+
+					this.theGrid[newI][newJ] = new Cell();
+					this.theGrid[newI][newJ].addElement(mainElement);
+				}
+			}
+		}
+
+		// loops for adding (Empty, Pad, Rock, Immovable)
 		for (int i = 0; i < this.length; i++) {
 			for (int j = 0; j < this.width; j++) {
-				
-				this.theGrid[i][j] = new Cell();
-				
-				int index = (int)(Math.random()*5);
-				String element = this.possibleElements[index];
-				if(element=="Teleportal")
-				{
-					if(!addedTeleportal)
-					{
-						addedTeleportal = true;
-						this.theGrid[i][j].addElement(element);
-					}
-					else
-					{ 
-						element = this.possibleElements[(int)Math.random()*4];
-						this.theGrid[i][j].addElement(element);
-					}
-					
+				System.out.println(this.theGrid[i][j]);
+				if (this.theGrid[i][j] == null) {
+					this.theGrid[i][j] = new Cell();
+					int index = (int) (Math.random() * 4);
+
+					String element = this.possibleElements[index];
+					this.theGrid[i][j].addElement(element);
 				}
-				else
-				{  
-					this.theGrid[i][j].addElement(element);	
-				}
-				
-				
+
 			}
 		}
 	}
 
-	
-	public void showGrid()
-	{
+	public void showGrid() {
 		for (int i = 0; i < this.length; i++) {
 			for (int j = 0; j < this.width; j++) {
-				
+
 				System.out.print(this.theGrid[i][j].toString() + " | ");
 			}
-			
+
 			System.out.println();
 			System.out.println();
-			}
+		}
 	}
-	
-	
-	public static void main(String [] args)
-	{
-		 Grid g = new Grid(3,9);
-		 
-		 g.initializeGrid();
-		 g.showGrid();
+
+	public static void main(String[] args) {
+		Grid g = new Grid(3, 3);
+
+		g.initializeGrid();
+		g.showGrid();
 
 	}
 
